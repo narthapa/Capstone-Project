7358e8f10edb099b8ed6d97b35571ceafb857fd2
Nar <nt3244@gmail.com>
2022-10-07 22:31:37 -0400

    update some codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
+import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    NavComponent
+    NavComponent,
+    HeaderComponent
+   
   ],
   imports: [
     BrowserModule  
